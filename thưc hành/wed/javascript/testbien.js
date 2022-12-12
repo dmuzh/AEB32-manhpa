8e45e1d46b830d59f31c79ad736bcda798230986
manh <muzh28@gmail.com>
2022-12-12 21:03:36 +0700

    homework8

---
+++ thưc hành/wed/javascript/testbien.js
@@ -0,0 +1,11 @@
+let arr =[ 10,11,16,19,20,22]
+let str = ' asdads aok   ' //string
+let objUser = {
+   name:'asdas',
+}
+let num = 10    
+console.log (Number,num);
+console.log (objUser, name)
+console.log(arr[3])
+console.log(String, str)
+let number1 =prompt ('please')
